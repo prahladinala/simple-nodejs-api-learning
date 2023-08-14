c12cf7a235cff7618e1805144543a97563f37b0d
Prahlad <prahladjngp@gmail.com>
2023-08-14 15:49:09 +0530

    ✅ instaSocial Object as Response - Type 1

--- index.js
+++ index.js
@@ -4,7 +4,18 @@ const app = express();
 const port = 3000 || process.env.PORT;
 
 app.get("/", (req, res) => {
-  res.send("<h1>Hello Prahlad!</h1>");
+  res.status(200).send("<h1>Hello Prahlad Inala</h1>");
+});
+
+// INSTAGRAM
+app.get("/api/v1/instagram", (req, res) => {
+  const instaSocial = {
+    username: "prahladinala",
+    followers: 1000,
+    follows: 100,
+    date: Date.now(),
+  };
+  res.status(200).json({ instaSocial });
 });
 
 app.listen(port, () => {
