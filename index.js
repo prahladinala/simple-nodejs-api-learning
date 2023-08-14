8a2b2444ae49c324cc79cd59df28c08383dfa1e9
Prahlad <prahladjngp@gmail.com>
2023-08-14 15:31:54 +0530

    ✅ Environment Variable Set if Available

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
-const port = 3000;
+
+const port = 3000 || process.env.PORT;
 
 app.get("/", (req, res) => {
   res.send("<h1>Hello Prahlad!</h1>");
